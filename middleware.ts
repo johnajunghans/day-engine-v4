c902829caac89ab1240c06e0bbb5625fbb818d0e
Tony Junghans <johnajunghans@gmail.com>
2025-01-24 19:18:41 -0800

    Attempted Fix: tried to allow CORS requests by including headers in the next.config.ts file

--- middleware.ts
+++ middleware.ts
@@ -1,28 +1,7 @@
-import { NextResponse, type NextRequest } from 'next/server'
+import { type NextRequest } from 'next/server'
 import { updateSession } from '@/lib/supabase/middleware'
 
-const allowedOrigins = [
-  'http://localhost:3000',
-  'https://www.dayengine.com',
-];
-
 export async function middleware(request: NextRequest) {
-  const res = NextResponse.next({ request })
-
-  const origin = request.headers.get('origin');
-
-  if(origin && allowedOrigins.includes(origin)) {
-    res.headers.append('Access-Control-Allow-Origin', origin)
-  }
-
-  // add the remaining CORS headers to the response
-  res.headers.append('Access-Control-Allow-Credentials', "true")
-  res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
-  res.headers.append(
-      'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-
   return await updateSession(request)
 }
 
@@ -36,7 +15,5 @@ export const config = {
      * Feel free to modify this pattern to include more paths.
      */
     '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
-    // Match all requests to /api
-    '/api/:path*', 
   ],
 }
