85ac6cbc5a271b7004859fa9c3dc15fecb168d10
Tony Junghans <johnajunghans@gmail.com>
2025-01-24 19:40:47 -0800

    one more attempt to fix CORS

--- next.config.ts
+++ next.config.ts
@@ -4,24 +4,17 @@ const nextConfig: NextConfig = {
   /* config options here */
   async headers() {
     return [
-      {
-        source: "/api/:path*",
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "https://www.dayengine.com", // Set your origin
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, POST, PUT, DELETE, OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type, Authorization",
-          },
-        ],
-      },
-    ];
+        {
+            // matching all API routes
+            source: "/api/:path*",
+            headers: [
+                { key: "Access-Control-Allow-Credentials", value: "true" },
+                { key: "Access-Control-Allow-Origin", value: "https://www.dayengine.com" }, 
+                { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+              ]
+          }
+      ]
   },
   images: {
     unoptimized: true
